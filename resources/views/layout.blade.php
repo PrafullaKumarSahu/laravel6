b6fe5b22e353bb446f5f1b013be6777716f0a332
prafullakumarsahu <prafullakumarsahu001@gmail.com>
2020-01-19 19:49:18 +0530

    Compiles assests

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,7 +19,7 @@ Released   : 20140225
 <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
 <link href="{{ asset('css/default.css')  }}" rel="stylesheet" type="text/css" media="all" />
 <link href="{{ asset('css/fonts.css') }}" rel="stylesheet" type="text/css" media="all" />
-
+<link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" media="all" />
 <!--[if IE 6]><link href="default_ie6.css" rel="stylesheet" type="text/css" /><![endif]-->
 
 </head>
@@ -42,5 +42,11 @@ Released   : 20140225
 <div id="wrapper">
 	<div id="page" class="container">
 		@yield('çontent')
+    </div>
+</div>
+  <div id="copyright" class="container">
+            <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+        </div>
+        <link href="{{ asset('js/app.js') }}" rel="stylesheet" type="text/css" media="all" />
 </body>
 </html>
