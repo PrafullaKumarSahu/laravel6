56546cd316cb28cddd3a7424853bb9c9923e2d1e
vagrant <vagrant@homestead>
2020-02-12 13:28:15 +0000

    Adds code related to container implementation

--- app/Http/Containers/Example.php
+++ app/Http/Containers/Example.php
@@ -3,6 +3,11 @@ namespace App\Http\Containers;
 
 class Example
 {
+    public function handle()
+    {
+        return 'It is working.';
+    }
+
     public function test()
     {
         return 'It is working.';
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,12 +2,13 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\Schema;
-
+use App\Http\Containers\Example;
 use App\Http\Containers\Example4;
+
 use App\Http\Containers\Example5;
 use App\Http\Containers\Collaborator;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,16 +19,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        app()->bind(Example4::class, function () {
+        app()->bind(Example4::class, function() {
             $collborator = new Collaborator;
             $extra = 'This can be moved inside controller just like any other logic can be moved to controller from these route closure';
             return new Example4($collborator, $extra);
         });
 
-        app()->singleton(Example5::class, function () {
+        app()->singleton(Example5::class, function() {
             $collborator = new Collaborator;
             $extra = 'This can be moved inside controller just like any other logic can be moved to controller from these route closure';
             return new Example4($collborator, $extra);
+        });
+
+        $this->app->bind('example', function(){
+            return new Example();
         });
     }
 
