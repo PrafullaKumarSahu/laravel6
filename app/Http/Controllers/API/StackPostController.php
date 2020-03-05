00940ea3f0b69ee9d23678927a5a7373e935aab0
prafullaallegiant <prafulla.sahu@allegiantair.com>
2020-03-05 13:00:10 +0530

    Reduce congintive complexities

--- app/Http/Controllers/API/StackPostController.php
+++ app/Http/Controllers/API/StackPostController.php
@@ -21,44 +21,51 @@ class StackPostController extends Controller
         //Next target to get accepted answer or top voted answer for all these posts,
         // may be questions most recent and top voted will be better
         try {
-            $fp = fopen('questions-15.csv', 'wb');
+            $fp = fopen('questions-test.csv', 'wb');
             $i = 0;
             $client = new Client();
 
-            $response = $client->get('https://api.stackexchange.com/2.2/questions?site=fitness&key=Gja2ijG6qGFPpdRJe0fK8A((&pagesize=50&page=15&q=[stretching]OR[warm-up]OR[martial-arts]OR[flexibility]&sort=activity&order=desc&min=2&is_answered=True&accepted=True');
+            $stackApiBaseUrl = 'https://api.stackexchange.com/2.2';
+            $key = 'Gja2ijG6qGFPpdRJe0fK8A((';
+            $questions = [];
+
+            $response = $client->get($stackApiBaseUrl . '/questions?site=fitness&key=' . $key . '&pagesize=50&page=15&q=[stretching]OR[warm-up]OR[martial-arts]OR[flexibility]&sort=activity&order=desc&min=2&is_answered=True&accepted=True');
         
             //For questions also put condition to get good questions based on upvotes
             if ($response->getStatusCode() == 200) {
                 foreach (json_decode($response->getBody())->items as $question) {
                     if ($question->answer_count > 0) {
-                    $questions = [];
-                    $questions['question_id'] = $question->question_id;
-                    $questions['title'] = $question->title;
-
-                   $answerResponse = $client->get('https://api.stackexchange.com/2.2/questions/' . $question->question_id . '/answers/?site=fitness&key=Gja2ijG6qGFPpdRJe0fK8A((&is_accepted=True');
-
-                   foreach (json_decode($answerResponse->getBody())->items as $answerId ) {
-                       if ($answerId->is_accepted == true || $answerId->score > 0) {
-                           $answerBodyResponse = $client->get('https://api.stackexchange.com/2.2/answers/' . $answerId->answer_id . '?&site=fitness&key=Gja2ijG6qGFPpdRJe0fK8A((&is_accepted=True&filter=withbody');
-
-                        foreach (json_decode($answerBodyResponse->getBody())->items as $item) {
-                                $questions['answer'] = strip_tags($item->body);
-                            }
-        
-                       }
-                   }
-
-                    if( $i === 0 ) {
-                        fputcsv($fp, array_keys($questions) ); // First write the headers
+                        $questions['question_id'] = $question->question_id;
+                        $questions['title'] = $question->title;
                     }
-                
-                    fputcsv($fp, $questions); // Then write the fields
-                
-                    $i++;
                 }
+
+                foreach ($questions as $question) {
+                    $answerResponse = $client->get($stackApiBaseUrl . '/questions/' . $question['question_id'] . '/answers/?site=fitness&key=' . $key . '&is_accepted=True');
+
+                    $answerBodyResponses = json_decode($answerResponse->getBody())->items;
                 }
+
+                foreach ($answerBodyResponses as $answerId ) {
+                    if ($answerId->is_accepted || $answerId->score > 0) {
+                        $answerBodyResponse = $client->get($stackApiBaseUrl . '/answers/' . $answerId->answer_id . '?&site=fitness&key=' . $key . '&is_accepted=True&filter=withbody');
+
+                        $answers = json_decode($answerBodyResponse->getBody())->items;
+                    }
+                }
+
+                foreach ($answers as $answer) {
+                    $questions['answer'] = strip_tags($answer->body);
+                }
+
+                if( $i === 0 ) {
+                    fputcsv($fp, array_keys($questions) ); // First write the headers
+                }
+            
+                fputcsv($fp, $questions); // Then write the fields
+            
+                $i++;
                 fclose($fp);
-                
             }
         } catch(RequestException $e) {
             dd($e);
